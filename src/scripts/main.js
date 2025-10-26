cd5da93c8938843231c5962d1e8a9059b8e55bf9
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-10-26 14:25:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const items = Array.from(list.children);
+
+const parseSalary = (salaryString) => {
+  return Number(salaryString.replace(/[$,]/g, ''));
+};
+
+const sortList = (liArray, ulLink) => {
+  liArray.sort((firstElement, secondElement) => {
+    const firstElementSalary = parseSalary(firstElement.dataset.salary);
+
+    const secondElementSalary = parseSalary(secondElement.dataset.salary);
+
+    return secondElementSalary - firstElementSalary;
+  });
+
+  ulLink.innerHTML = '';
+  liArray.forEach((item) => ulLink.append(item));
+};
+
+const getEmployees = (liArray) => {
+  return liArray.map((li) => ({
+    name: li.textContent.trim(),
+    position: li.dataset.position,
+    salary: parseSalary(li.dataset.salary),
+    age: Number(li.dataset.age),
+  }));
+};
+
+sortList(items, list);
+getEmployees(items);
